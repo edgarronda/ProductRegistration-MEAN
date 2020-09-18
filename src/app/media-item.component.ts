9b01598440b14c70fa958e3149f3485ed8defdbe
Edgar Ronda <info@edgarantonio.com>
2020-09-18 13:31:23 -0700

    Adding products list in the dashboard.

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  @Input() productItem;
+  @Output() delete = new EventEmitter();
+
+  onRegister(){
+    console.log("Test Registration");
+  }
+  onDelete(){
+    console.log("Test Remove");
+    this.delete.emit(this.productItem);
+  }
+}
